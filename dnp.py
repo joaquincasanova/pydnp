284dc8d277920622659c18dbac4f8f91d9e86e38
joaquincasanova <joaquin.casanova@gmail.com>
2014-05-25 13:40:02 +0000

    corrections

--- dnp.py
+++ dnp.py
@@ -9,8 +9,8 @@ import attenuator
 freq = float(sys.argv[1]) #MHz
 attn = float(sys.argv[2])
 resolution = 500000  
+attenuator.digatt_set(attn)
+print " I just set the digatt."
 
 synthesizer.yig_set_freq(freq, resolution)
 print " I just set the yig."
-attenuator.digatt_set(attn)
-print " I just set the digatt."
